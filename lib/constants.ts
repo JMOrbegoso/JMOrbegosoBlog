f474c820b2975f2786ca1298ab6cdd431b4a0463
JMOrbegoso <dev@jmorbegoso.com>
2021-01-19 16:27:38 -0500

    Updated constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,13 @@ export const EXAMPLE_PATH = 'blog-starter-typescript';
 export const CMS_NAME = 'Markdown';
 export const HOME_OG_IMAGE_URL =
   'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
+export const WEB_NAME = "JM Orbegoso's Blog";
+export const CREDITS = 'Developed by JM Orbegoso';
+export const COPYRIGHT = '© 2021 JM Orbegoso';
+export const PERSONAL_WEB = 'https://www.jmorbegoso.com';
+export const FACEBOOK = '';
+export const TWITTER = 'JMOrbegosoDev';
+export const LINKEDIN = 'jmorbegoso';
+export const GITHUB = 'JMOrbegoso';
+export const YOUTUBE = '';
+export const INSTAGRAM = '';
