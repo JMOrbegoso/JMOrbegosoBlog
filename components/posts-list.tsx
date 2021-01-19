6a665a29146188bcb5c9dd1d92a0441a8ea70c41
JMOrbegoso <dev@jmorbegoso.com>
2021-01-19 17:19:15 -0500

    Updated and renamed MorePosts to PostsList component

--- components/posts-list.tsx
+++ components/posts-list.tsx
@@ -5,13 +5,10 @@ type Props = {
   posts: Post[];
 };
 
-const MoreStories = ({ posts }: Props) => {
+const PostsList = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
@@ -21,6 +18,7 @@ const MoreStories = ({ posts }: Props) => {
             author={post.author}
             slug={post.slug}
             excerpt={post.excerpt}
+            tags={post.tags}
           />
         ))}
       </div>
@@ -28,4 +26,4 @@ const MoreStories = ({ posts }: Props) => {
   );
 };
 
-export default MoreStories;
+export default PostsList;
