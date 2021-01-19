50e178e91535ef56bd7ad4539b1d3d1ffa9fec08
JMOrbegoso <dev@jmorbegoso.com>
2021-01-19 17:30:18 -0500

    Updated index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from '../components/container';
-import MoreStories from '../components/more-stories';
-import HeroPost from '../components/hero-post';
-import Intro from '../components/intro';
+import PostsList from '../components/posts-list';
 import Layout from '../components/layout';
 import { getAllPosts } from '../lib/api';
 import Head from 'next/head';
-import { CMS_NAME } from '../lib/constants';
+import { WEB_NAME } from '../lib/constants';
 import Post from '../types/post';
 
 type Props = {
@@ -13,27 +12,14 @@ type Props = {
 };
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title> {WEB_NAME} </title>
         </Head>
         <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <PostsList posts={allPosts} />}
         </Container>
       </Layout>
     </>
