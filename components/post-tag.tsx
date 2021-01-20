c1bc4afa7e3734a5bd0a737b4f5283fc1ec9ab35
JMOrbegoso <dev@jmorbegoso.com>
2021-01-20 11:25:02 -0500

    Created tag-helpers.ts

--- components/post-tag.tsx
+++ components/post-tag.tsx
@@ -1,16 +1,7 @@
 import Link from 'next/link';
 import { PostTag as PostTagEnum } from '../lib/enums/postTag';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faNode,
-  faReact,
-  faAngular,
-  faYarn,
-  faLinux,
-} from '@fortawesome/free-brands-svg-icons';
-
-library.add(faNode, faReact, faAngular, faYarn, faLinux);
+import { getTagTitle, getFontAwesomeIcon } from '../lib/tag-helpers';
 
 type Props = {
   tag: PostTagEnum;
@@ -50,41 +41,5 @@ const PostTag = ({ tag }: Props) => {
     </>
   );
 };
-
-function getFontAwesomeIcon(tag: PostTagEnum) {
-  switch (tag) {
-    case PostTagEnum.Node:
-      return faNode;
-    case PostTagEnum.React:
-      return faReact;
-    case PostTagEnum.Angular:
-      return faAngular;
-    case PostTagEnum.Yarn:
-      return faYarn;
-    case PostTagEnum.WSL:
-      return faLinux;
-
-    default:
-      throw new Error(`Invalid tag: ${tag}`);
-  }
-}
-
-function getTagTitle(tag: PostTagEnum) {
-  switch (tag) {
-    case PostTagEnum.Node:
-      return 'Node JS';
-    case PostTagEnum.React:
-      return 'React JS';
-    case PostTagEnum.Angular:
-      return 'Angular';
-    case PostTagEnum.Yarn:
-      return 'Yarn';
-    case PostTagEnum.WSL:
-      return 'WSL';
-
-    default:
-      throw new Error(`Invalid tag: ${tag}`);
-  }
-}
 
 export default PostTag;
