e87afc7f04fd7a22ff1664d79daf1adb62cbbb8a
JMOrbegoso <dev@jmorbegoso.com>
2021-01-20 11:57:13 -0500

    Updated PostHeader component

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -16,17 +16,25 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+        <Avatar
+          name={`${author.firstname} ${author.lastname}`}
+          picture={author.picture}
+          web={author.web}
+        />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
+          <Avatar
+            name={`${author.firstname} ${author.lastname}`}
+            picture={author.picture}
+            web={author.web}
+          />
         </div>
         <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
+          🗓 <DateFormatter dateString={date} />
         </div>
       </div>
     </>
