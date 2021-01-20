8cd5e81526b0e5bacba909c31610b4799d0ea52d
JMOrbegoso <dev@jmorbegoso.com>
2021-01-20 11:56:56 -0500

    Updated Avatar component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,14 +1,17 @@
 type Props = {
   name: string;
   picture: string;
+  web: string;
 };
 
-const Avatar = ({ name, picture }: Props) => {
+const Avatar = ({ name, picture, web }: Props) => {
   return (
-    <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
-      <div className="text-xl font-bold">{name}</div>
-    </div>
+    <a href={web} target="_blank">
+      <div className="flex items-center">
+        <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+        <div className="text-xl font-bold">{name}</div>
+      </div>
+    </a>
   );
 };
 
