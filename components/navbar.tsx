b2f027c6f5b19f2dad0840d47a56ca0547d6b191
JMOrbegoso <dev@jmorbegoso.com>
2021-01-23 13:09:44 -0500

    Updated to use ILocalResources

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import LanguagePicker from './language-picker';
+import ILocalResources from '../interfaces/ilocalresources';
 
 type Props = {
   title: string;
+  localResources: ILocalResources;
 };
 
-const NavBar = ({ title }: Props) => {
+const NavBar = ({ title, localResources }: Props) => {
   return (
     <Navbar bg="primary" variant="dark" fixed="top" expand="lg">
       <Navbar.Brand href="/">{title}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/tags">Tags</Nav.Link>
-          <Nav.Link href="/about">About Me</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link href="/">{localResources.home}</Nav.Link>
+          <Nav.Link href="/tags">{localResources.tags}</Nav.Link>
+          <Nav.Link href="/about">{localResources.about}</Nav.Link>
+          <Nav.Link href="/contact">{localResources.contact}</Nav.Link>
         </Nav>
         <LanguagePicker />
       </Navbar.Collapse>
