0c9ec6601c59d0425e4e49c65ba634f5be18e998
JMOrbegoso <dev@jmorbegoso.com>
2021-01-23 09:47:04 -0500

    Updated NavBar adding the LanguagePicker component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import LanguagePicker from './language-picker';
 
 type Props = {
   title: string;
@@ -18,6 +19,7 @@ const NavBar = ({ title }: Props) => {
           <Nav.Link href="/about">About Me</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
+        <LanguagePicker />
       </Navbar.Collapse>
     </Navbar>
   );
