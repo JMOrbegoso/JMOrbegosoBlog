6e98d8b4202f2901415f467e9dc06080e7213a9f
JMOrbegoso <dev@jmorbegoso.com>
2021-01-25 12:10:11 -0500

    Updated constants.ts adding URL_BASE and DISQUS_SHORTNAME values

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,8 +2,10 @@ export const EXAMPLE_PATH = 'blog-starter-typescript';
 export const CMS_NAME = 'Markdown';
 export const HOME_OG_IMAGE_URL =
   'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
+export const URL_BASE = 'https://blog.jmorbegoso.com';
 export const WEB_NAME = "JM Orbegoso's Blog";
 export const CREDITS = 'JM Orbegoso';
 export const COPYRIGHT = '© 2021 JM Orbegoso';
 export const POST_PER_PAGE = 6;
 export const PAGINATION_LENGTH = 2;
+export const DISQUS_SHORTNAME = 'your-disqus-shortname';
