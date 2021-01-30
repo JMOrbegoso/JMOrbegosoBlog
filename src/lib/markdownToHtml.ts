60f5df3aed6e6bba060a1ed350b25e648428db72
JMOrbegoso <dev@jmorbegoso.com>
2021-01-30 12:02:04 -0500

    Updated markdownToHtml to use remark-prism plugins

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -3,7 +3,9 @@ import remark from 'remark';
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(require('remark-html'))
-    .use(require('remark-prism'))
+    .use(require('remark-prism'), {
+      plugins: ['line-numbers', 'treeview'],
+    })
     .process(markdown);
   return result.toString();
 }
