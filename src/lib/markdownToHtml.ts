8dc23d523ba5d41f41b5e54837599f81dfda3613
JMOrbegoso <dev@jmorbegoso.com>
2021-01-30 10:00:59 -0500

    Updated markdownToHtml to use remark-prism

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,9 @@
 import remark from 'remark';
-import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(require('remark-html'))
+    .use(require('remark-prism'))
+    .process(markdown);
   return result.toString();
 }
