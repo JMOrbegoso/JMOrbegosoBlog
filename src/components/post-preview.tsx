32b86483a3be11be3a13270688c6208f7553e78b
JMOrbegoso <dev@jmorbegoso.com>
2021-01-31 13:04:26 -0500

    Updated styles of PostPreview component

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -28,8 +28,8 @@ const PostPreview = ({
   localResources,
 }: Props) => {
   return (
-    <div>
-      <h3 className="text-3xl mb-3 leading-snug">
+    <div className="my-3">
+      <h3 className="text-3xl mb-3 leading-snug text-center">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
@@ -47,7 +47,7 @@ const PostPreview = ({
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className="text-lg leading-relaxed mb-4 text-justify">{excerpt}</p>
       <PostTags tags={tags} />
     </div>
   );
