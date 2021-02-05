988606de18c8f203cc16b58153604945fb8a9e0a
JMOrbegoso <dev@jmorbegoso.com>
2021-02-05 09:41:11 -0500

    Implemented posts with date as name prefix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,13 +15,58 @@ export function getPostSlugs(locale: string) {
   return fs.readdirSync(postsDirectory(locale));
 }
 
-export function getPostBySlug(
+function getPostFullPath(locale: string, slug: string) {
+  const dir = postsDirectory(locale);
+  const postsFiles = fs.readdirSync(dir);
+  const regex = new RegExp(`([0-9]{4}-[0-9]{2}-[0-9]{2}-${slug}.md)`);
+  const post = postsFiles.find((postFile) => regex.test(postFile));
+  return post;
+}
+
+export function getPostByFilePath(
   locale: string,
   slug: string,
   fields: string[] = [],
 ) {
   const realSlug = slug.replace(/\.md$/, '');
   const fullPath = join(postsDirectory(locale), `${realSlug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+
+  type Items = {
+    [key: string]: string;
+  };
+
+  const items: Items = {};
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === 'slug') {
+      items[field] = realSlug.slice('xxxx-xx-xx-'.length);
+    }
+    if (field === 'content') {
+      items[field] = content;
+    }
+
+    if (data[field]) {
+      items[field] = data[field];
+    }
+  });
+
+  return items;
+}
+
+export function getPostBySlug(
+  locale: string,
+  slug: string,
+  fields: string[] = [],
+) {
+  const realSlug = slug.replace(/\.md$/, '');
+  const fullName = getPostFullPath(locale, slug);
+  if (!fullName) {
+    return {};
+  }
+  const fullPath = join(postsDirectory(locale), fullName);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
@@ -84,7 +129,7 @@ export function getAuthorBySlug(
 export function getAllPosts(locale: string, fields: string[] = []) {
   const slugs = getPostSlugs(locale);
   const posts = slugs
-    .map((slug) => getPostBySlug(locale, slug, fields))
+    .map((slug) => getPostByFilePath(locale, slug, fields))
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
