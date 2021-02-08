5e17403b6665d98cf395c3bd1b68c8e9f0733b42
JMOrbegoso <dev@jmorbegoso.com>
2021-02-08 07:30:28 -0500

    Updated PostPreview component to use React Bootstrap Card

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -1,9 +1,9 @@
-import DateFormatter from './date-formatter';
 import Link from 'next/link';
+import DateFormatter from './date-formatter';
 import { PostTag as PostTagEnum } from '../enums/postTag';
 import PostTags from './post-tags';
-import CoverImage from './cover-image';
 import ReadTime from './read-time';
+import { Card } from 'react-bootstrap';
 
 type Props = {
   title: string;
@@ -25,28 +25,36 @@ const PostPreview = ({
   tags,
 }: Props) => {
   return (
-    <div className="my-3">
-      <h3 className="text-3xl mb-3 leading-snug text-center">
+    <Card className="m-2" style={{ minWidth: 300 }}>
+      <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <a aria-label={title}>
+          <Card.Img variant="top" src={coverImage} />
+        </a>
+      </Link>
+
+      <Card.Body>
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+          <a className="hover:underline">
+            <Card.Title className="text-center">{title}</Card.Title>
+          </a>
         </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <div className="row">
-          <div className="col-6 text-left">
-            <DateFormatter dateString={date} />
-          </div>
-          <div className="col-6 text-right">
-            <ReadTime content={content} />
+        <div className="mb-2">
+          <div className="row">
+            <div className="col-6 text-left">
+              <DateFormatter dateString={date} />
+            </div>
+            <div className="col-6 text-right">
+              <ReadTime content={content} />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4 text-justify">{excerpt}</p>
-      <PostTags tags={tags} />
-    </div>
+        <Card.Text className="text-justify">{excerpt}</Card.Text>
+      </Card.Body>
+
+      <Card.Footer>
+        <PostTags tags={tags} />
+      </Card.Footer>
+    </Card>
   );
 };
 
